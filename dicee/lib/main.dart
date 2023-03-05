2cafa7d129e90ef3082a1929e8be404c4da7a381
Sean <sean.lubbers@gmail.com>
2023-03-05 11:26:00 -0800

    Updated app title

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -7,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dice App'),
           backgroundColor: Colors.red,
           centerTitle: true,
         ),
