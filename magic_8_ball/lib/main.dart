cca47071542de263e241d1af5726c7e9c58f958e
Sean <sean.lubbers@gmail.com>
2023-03-06 22:12:08 -0800

    Built stateful widget to handle ball state

--- magic_8_ball/lib/main.dart
+++ magic_8_ball/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
   MaterialApp(
@@ -16,10 +17,40 @@ class BallPage extends StatelessWidget {
           backgroundColor: Colors.blue[900],
         ), //AppBar
         backgroundColor: Colors.blue,
-        body: Container(
-          
-        )
+        body: Ball()
       ),
     );
   }
 }
+
+class Ball extends StatefulWidget {
+  const Ball({ super.key });
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  var ballNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: (){
+                Random random = new Random();
+                ballNumber = 1 + random.nextInt(5);
+                // print("Ball Number is $ballNumber");
+                print("ballNumber is $ballNumber");
+              },
+              child: Center(child: Image.asset('images/ball$ballNumber.png')),
+            ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
